0903c6f5637597349182b54de307ad8702b3d4b2
willendary <59053072+willendary@users.noreply.github.com>
2019-12-23 15:29:50 -0300

    Add files via upload

--- jscode.js
+++ jscode.js
@@ -1,9 +1,7 @@
 var nome = ""
 
-function NumeroAleatorio() {
-    var aleatorio = Math.random();
-        alert("Um número aleatório: " + aleatorio);
-    nome = window.prompt("Qual o seu nome");
+function ObtemNome() {
+        nome = window.prompt("Qual o seu nome");
 }
 
 function MostraNome(){
@@ -16,9 +14,39 @@ if (nome=="Willian"){
 }
 
 function SomaValores(){
-var num2 = Number(document.getElementById("n1").value);
-var num1 = Number(document.getElementById("n2").value);
+var num1 = Number(document.getElementById("n1").value);
+var num2 = Number(document.getElementById("n2").value);
 var soma = num1 + num2;
 alert("A soma dos valores é " + soma);
+}
+
+function MultiplicaValores(){
+    var num1 = Number(document.getElementById("n1").value);
+    var num2 = Number(document.getElementById("n2").value);
+    var multiplicar = num1 * num2;
+    alert("A multiplicação dos valores é " + multiplicar);
+    }
+
+function SubtraiValores(){
+    var num1 = Number(document.getElementById("n1").value);
+    var num2 = Number(document.getElementById("n2").value);
+    var subtrair = num1 - num2;
+        alert("A subtração dos valores é " + subtrair);
+        }
+      
+function DivideValores(){
+    var num1 = Number(document.getElementById("n1").value);
+    var num2 = Number(document.getElementById("n2").value);
+    var divisao = num1/num2;
+    alert("A divisão dos valores é " + divisao);
+    //window.prompt("O valor da divisão é",divisao) - também pode ser usado no lugar do alert()
+}
+
+function GeraNumero() {
+    var aleatorio1 = Math.floor(Math.random() * 1000);
+    var    aleatorio2 =  Math.floor(Math.random() * 1000);
+        (document.getElementById("n1").value) = aleatorio1;
+        (document.getElementById("n2").value) = aleatorio2;
+    
 }
 
